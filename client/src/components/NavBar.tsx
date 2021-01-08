9a611f907643481319a3c40e8e700404bc0faea4
DeeDowns <dee_downs@outlook.com>
2021-01-07 20:13:52 -0500

    create NavBar component add to App

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+const NavBar: React.FC = () => {
+    
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default NavBar
