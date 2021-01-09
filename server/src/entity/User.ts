9d68ff55eedd13af1c3898753c5f20addb5d3060
DeeDowns <dee_downs@outlook.com>
2021-01-09 11:07:46 -0500

    set up tables

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,18 +1,28 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Post } from './Post'
 
 @Entity()
 export class User {
 
     @PrimaryGeneratedColumn()
     id: number;
+ 
+    @Column({ nullable: true })
+    firstName: string | null;
 
-    @Column()
-    firstName: string;
+    @Column({ nullable: true })
+    lastName: string | null;
 
-    @Column()
-    lastName: string;
+    @Column({ unique: true, nullable: false})
+    username: string;
 
-    @Column()
-    age: number;
+    @Column({ unique: true, nullable: false})
+    email: string;
+
+    @Column({ nullable: false })
+    password: string
+
+    @OneToMany(() => Post, post => post.user)
+    post: Post[]
 
 }
