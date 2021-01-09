14c1587866a02591ebdd8ca974d1eb9e8ee392f0
DeeDowns <dee_downs@outlook.com>
2021-01-09 18:35:40 -0500

    set up routes

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,18 +1,47 @@
 import { Router, Request, Response } from 'express'
-// authController
-// checkJwt
+// import jwt from 'jsonwebtoken'
+import bycrypt from 'bcryptjs'
+import db from '../db'
+
+
+
 
 const router = Router()
 
-router.get('/register', (req: Request, res: Response) => {
-    console.log(req.method)
-    res.status(200).json({ message: 'ok'})
+
+router.post('/login', (req: Request, res: Response) => {
+    db('user').select()
+    .where({ username: req.body.username})
+    .first()
+    .then((user: any ) => {
+        console.log(user)
+        if(user && bycrypt.compareSync(req.body.password, user.password)) {
+            res.cookie('loggedIn', 'true')
+            // res.redirect('/feed')
+            console.log(res.cookie)
+        }
+    })
+    .catch((err: any) => {
+        console.log(err)
+        res.redirect('/login')
+    })
 })
 
-router.get('/login', (req: Request, res: Response) => {
-    console.log(req.method)
-    res.status(200).json({ message: 'ok'})
-})
 
+// function makeJwt( { id, username }: any) {
+//     const payload = {
+//         username,
+//         subject: id
+//     }
+//     const secret = '{
+//         jwtSecret: 'super secret sauce'
+//     }
+//     const options = {
+//         expiresIn: '2 hours'
+//     }
+
+//     return jwt.sign(payload, secret.jwtSecret, options)
+
+// }
 
 export default router 
