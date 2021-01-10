a41f89d2550ec99d48cd67a64226be61ae6b6d1a
DeeDowns <dee_downs@outlook.com>
2021-01-10 17:47:31 -0500

    fix issues with backend so user endpoints work

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -6,15 +6,14 @@ const router = Router()
 
 router.post('/', (req: Request, res: Response) => {
     // check req body
-    console.log(req.body.username, req.body.password, req.body.email)
-    const { username, email, password } = req.body
     const credentials = req.body
     const hashPassword = bycrypt.hashSync(credentials.password, 8)
-    credentials.password =hashPassword
+    credentials.password = hashPassword
 
     db('user')
-    .insert({username: username, password: password, email: email}, ['id'])
-    .then((_user: any) => {
+    .insert(credentials, 'id')
+    .then((user: any) => {
+        console.log('USER',user[0])
         res.status(200).send('new user registered')
     })
     
