ca2683474b08b6438b0ce7973737c9af470d22a3
DeeDowns <dee_downs@outlook.com>
2021-01-13 07:31:47 -0500

    set up server tests with jest

---
+++ server/src/__test__/app.test.ts
@@ -0,0 +1,23 @@
+// import users from '../routes/users'
+import app from '../app'
+// import db from '../db'
+const supertest = require('supertest')
+
+describe('/', () => {
+    describe('GET to /', () => {
+        it('should return a http status code 200 when making a get request to the base url', () => {
+            return supertest(app)
+            .get('/')
+            .then((res:any) => {
+                expect(res.status).toBe(200)
+            })
+        })
+        it('should return a json object with a welcome message', () => {
+            return supertest(app)
+            .get('/')
+            .then((res:any) => {
+                expect(res.body.message).toBe('Server Uppppp')
+            })
+        })
+    })
+})
